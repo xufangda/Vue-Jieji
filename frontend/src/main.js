e9e72b28ae3c8e1669283ec8e455a574bb0e0127
fangda <poi998998@gmail.com>
2018-04-15 01:14:29 +0100

    init commit

---
+++ frontend/src/main.js
@@ -0,0 +1,22 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+import 'styles/reset.css' // 统一各浏览器表现的基本css
+import 'styles/border.css' // 解决高倍评一像素边框的问题
+
+Vue.config.productionTip = false
+fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
