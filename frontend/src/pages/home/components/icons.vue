cd64a1701cebf2045caabf4b5183068cb8c2d9ed
fangda <poi998998@gmail.com>
2018-04-15 23:51:19 +0100

    icons

---
+++ frontend/src/pages/home/components/icons.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class='icons'>
+    <swiper :options='swiperOption'>
+      <!-- slides -->
+      <swiper-slide v-for='page of pages' :key='page.index'>
+        <div
+          class='icon'
+          v-for='item of page'
+          :key='item.id'
+        >
+          <div class='icon-img'>
+            <img class='icon-img-content' :src='item.imgUrl'>
+          </div>
+          <p class='icon-desc'>{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      swiperOption: {
+        autoplay: false
+      },
+      iconList: [{
+        id: '0001',
+        imgUrl: require('styles/icons/pick.png'),
+        desc: '接机/送机'
+      }, {
+        id: '0002',
+        imgUrl: require('styles/icons/friend.png'),
+        desc: '结伴出行'
+      }, {
+        id: '0003',
+        imgUrl: require('styles/icons/moving.png'),
+        desc: '搬家用车'
+      }, {
+        id: '0004',
+        imgUrl: require('styles/icons/translate.png'),
+        desc: '商务翻译'
+      }
+      ]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 4)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+@import '~styles/varibles.styl'
+@import '~styles/mixins.styl'
+.icons >>>.swiper-container
+  height: 0
+  padding-bottom: 25%
+.icons
+  min-width 6.2rem
+  margin-top:.1rem
+  .icon
+    height : 0
+    position: relative
+    overflow: hidden
+    float: left
+    width: 25%
+    padding-bottom:25%
+    .icon-img
+      position:absolute
+      top: 0
+      left: 0
+      right: 0
+      bottom: .44rem
+      box-sizing:border-box
+      padding: .1rem
+      .icon-img-content
+        display: block
+        margin: 0 auto
+        height: 100%
+    .icon-desc
+      position: absolute
+      left: 0
+      right: 0
+      bottom:0
+      height : .44rem
+      line-height : .44rem
+      setFontfamily()
+      font-weight bold
+      color: $darkTextColor
+      text-align: center
+      ellipsis()
+</style>
--- frontend/src/pages/home/home.vue
+++ frontend/src/pages/home/home.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
+  <div class="home">
     <home-header></home-header>
+    <home-icons></home-icons>
     <!-- <home-picker></home-picker> -->
   </div>
 </template>
@@ -8,16 +9,19 @@
 <script>
 import HomeHeader from './components/header'
 import HomePicker from './components/picker'
+import HomeIcons from './components/icons'
 export default {
   name: 'home',
   components: {
     HomeHeader,
-    HomePicker
+    HomePicker,
+    HomeIcons
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-body
-  font-family "PingFang-SC", "Hiragino Sans GB", "微软雅黑", "Microsoft YaHei", "Heiti SC"
+  .home
+    font-family "PingFang-SC", "Hiragino Sans GB", "微软雅黑", "Microsoft YaHei", "Heiti SC"
+    min-width 6.4rem
 </style>
